6e990938ca741fa7bc277e8c3687538d0ef98130
IngEdwinVasquez <40243300+IngEdwinVasquez@users.noreply.github.com>
2025-05-14 01:47:16 -0400

    Update __manifest__.py

--- facturacion-electronica/point_of_sale_custom/__manifest__.py
+++ facturacion-electronica/point_of_sale_custom/__manifest__.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Point Of Sale Custom",
-
-    'summary': "",
-
-    'description': """
-Herencia de point_of_sale
-    """,
-
-    'author': "Gritvap",
-    "contribuitors": [
-        "Moisés Sia <msira2709.dev@gmail.com>",
-        "Eduardo Bolivar <eduardobolivar2407@gmail.com>"
-    ],
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'name': 'Point Of Sale ECF',
+    'version': '17.0.1.0.0',
+    'license': 'LGPL-3',
     'depends': ['point_of_sale'],
+    'category': 'Accounting',
+    'summary': 'Personalizaciones para el modelo de facturas',
+    'installable': True,
+    'application': False,
 
     # always loaded
     'data': [
